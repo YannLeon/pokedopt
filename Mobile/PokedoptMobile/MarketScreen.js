1e7e9a958807cfdf0e0fc51e1eba45cd5d87dcb9
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2022-02-03 15:39:00 +0100

    debut de pokemon card

--- Mobile/PokedoptMobile/MarketScreen.js
+++ Mobile/PokedoptMobile/MarketScreen.js
@@ -1,9 +1,16 @@
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import React from 'react';
+import { PokemonCard } from './PokemonCard';
 export const MarketScreen = () => {
   return (
     <View>
       <Text style={{textAlign: 'center', color: 'black'}}>AMRKET</Text>
+      <View>
+        <FlatList
+          data={['kangaskhan','spheal']}
+          renderItem={(item) => {<PokemonCard name={item}/>}}
+        />
+      </View>
     </View>
   );
 };
