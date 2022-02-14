bca53347872ebc169181cb09c159104701a15193
YannLeon <54958888+YannLeon@users.noreply.github.com>
2022-02-14 18:04:54 +0100

    add a button to contact vendor adaptativ

--- Mobile/PokedoptMobile/MarketScreen.js
+++ Mobile/PokedoptMobile/MarketScreen.js
@@ -1,14 +1,14 @@
 import {FlatList, Text, View} from 'react-native';
 import React from 'react';
 import { PokemonCard } from './PokemonCard';
-export const MarketScreen = () => {
+export const MarketScreen = (props) => {
   return (
     <View>
       <Text style={{textAlign: 'center', color: 'black'}}>AMRKET</Text>
       <View style={{marginBottom:45}}>
         <FlatList
           data={['kangaskhan','spheal','charizard','gyarados','magikarp','dialga','mewtwo','mew','celebi','tentacool','chikorita','turtwig','piplup','kangaskhan','kangaskhan','kangaskhan','kangaskhan','kangaskhan','kangaskhan']}
-          renderItem={({item}) => (<PokemonCard name={item}/>)}
+          renderItem={({item}) => (<PokemonCard name={item} navigation={props.navigation}/>)}
         />
       </View>
     </View>
--- Mobile/PokedoptMobile/PokemonCard.js
+++ Mobile/PokedoptMobile/PokemonCard.js
@@ -1,27 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View,Image,Button, Alert} from 'react-native';
+import {Text, View, Image, Button, Alert} from 'react-native';
 import {Card} from 'react-native-elements';
 import {black} from 'react-native-paper/lib/typescript/styles/colors';
-export const PokemonCard = props =>{
-    const [picture, setPicture] = useState(
-        'https://art.ngfiles.com/images/386000/386577_stardoge_8-bit-pokeball.png?f1446737358',
-      );
-      useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/' + props.name)
-          .then(response => response.json())
-          .then(responseJSON => {
-            setPicture(responseJSON.sprites.other["official-artwork"].front_default)
-          }).catch(e=>console.log(e));
-
-      }, [props.name]);
-    return(
-        <View style={{justifyContent: 'center',alignItems: 'center',}}>
-            <Card>
-              <Text style={{color:"black", textAlign: 'center',fontWeight:'bold',textTransform: 'uppercase'}} >{props.name}</Text>
-                <Image style={{width: 100, height: 100}} source ={{uri:picture}}/>
-                
-                <Button onPress={()=>console.log(props.name + " just got bought")} title="Adopt it!"/>
-            </Card>        
-        </View>
-    )
-}
+export const PokemonCard = props => {
+  const [picture, setPicture] = useState(
+    'https://art.ngfiles.com/images/386000/386577_stardoge_8-bit-pokeball.png?f1446737358',
+  );
+  const [response, setResponse] = useState();
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + props.name)
+      .then(response => response.json())
+      .then(responseJSON => {
+        setPicture(
+          responseJSON.sprites.other['official-artwork'].front_default,
+        );
+        setResponse(responseJSON);
+      })
+      .catch(e => console.log(e));
+  }, [props.name]);
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Card>
+        <Text
+          style={{
+            color: 'black',
+            textAlign: 'center',
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+          }}>
+          {props.name}
+        </Text>
+        <Image style={{width: 100, height: 100}} source={{uri: picture}} />
+        <Button
+          onPress={() =>
+            props.navigation.navigate('Pokemon', {
+              item: response,
+            })
+          }
+          title="Adopt it!"
+        />
+      </Card>
+    </View>
+  );
+};
